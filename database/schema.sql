445d88eca0e8891deb1a69317c5624152f3dd587
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-10-30 22:18:57 -0700

    Created database tables.

---
+++ database/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS  firm_db;
+CREATE DATABASE firm_db;
+
+USE firm_db;
+
+CREATE TABLE firm_department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE firm_role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES firm_department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE firm_employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) 
+    REFERENCES firm_role(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) 
+    REFERENCES firm_employee(id)
+    ON DELETE SET NULL
+);
